2a290cbc8b48ff7c5fabfcc36c9298e5896b6572
Sang Kibet Elkana <elkanahkibet6@gmail.com>
2023-07-02 14:10:38 +0300

    Created table guest

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+import datetime
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -8,7 +9,19 @@ class Episode(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.String(10), nullable=False)
     number = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
     appearances = db.relationship('Appearance', backref='episode', cascade='all, delete-orphan')
 
+class Guest(db.Model):
+    __tablename__ = 'guest'
 
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    occupation = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    appearances = db.relationship('Appearance', backref='guest', cascade='all, delete-orphan')
 # add any models you may need. 
