b340ac7a9548839f17c9f52a5e0e1e9e37c5d565
Sang Kibet Elkana <elkanahkibet6@gmail.com>
2023-07-02 14:27:26 +0300

    Models created and migrated

--- app/models.py
+++ app/models.py
@@ -4,13 +4,13 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Episode(db.Model):
-    __tablename__ = 'Episode'
+    __tablename__ = 'episode'
 
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.String(10), nullable=False)
     number = db.Column(db.Integer, nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     appearances = db.relationship('Appearance', backref='episode', cascade='all, delete-orphan')
 
@@ -21,18 +21,17 @@ class Appearance(db.Model):
     rating = db.Column(db.Integer, nullable=False)
     episode_id = db.Column(db.Integer, db.ForeignKey('episode.id'), nullable=False)
     guest_id = db.Column(db.Integer, db.ForeignKey('guest.id'), nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    
 class Guest(db.Model):
     __tablename__ = 'guest'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
     occupation = db.Column(db.String(255), nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     appearances = db.relationship('Appearance', backref='guest', cascade='all, delete-orphan')
 # add any models you may need. 
